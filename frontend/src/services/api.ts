60a4208eba7afc18b870e1338bc6032b3352382a
vale150422 <katherin.vasquez@correounivalle.edu.co>
2026-06-01 17:28:47 -0500

    actualizacion de server

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE = 'http://localhost:3001/api';
+const BASE = 'https://6a1e010bdc51a9df8399f161--agrogestioninventario.netlify.app/i';
 
 function getHeaders() {
   const token = localStorage.getItem('token');
