39c31a4e771122d59777a6619fbb1cddedbdefe4
vale150422 <katherin.vasquez@correounivalle.edu.co>
2026-06-04 17:52:49 -0500

    actualizacion dashboard

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+
 interface Props {
   children: React.ReactNode;
 }
 
 export default function DashboardLayout({ children }: Props) {
   return (
-    <div className="flex-1 overflow-auto">
+    <div className="flex-1 overflow-y-auto overflow-x-hidden">
       {children}
     </div>
   );
